1b7d15f93512ace6cf3d065c03a5e41639a74f7c
Shay Abraham <shaykos@gmail.com>
2026-03-16 10:01:48 +0200

    Initial commit

---
+++ קבוצה 14/TIRGUL_1/Example_1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Console.WriteLine("Hello, World!");
+
+// //Console.Write --> מדפיס את הטקסט על המסך בלי לרדת שורה
+// Console.Write("Enter the first number: ");
+// //Console.ReadLine() --> קורא קלט מהמשתמש ומחזיר אותו כמחרוזת
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Enter the second number: ");
+// int b = int.Parse(Console.ReadLine());
+
+// //Console.WriteLine("The sum of {a} and {b} is {a + b}.");
+// Console.WriteLine($"The sum of {a} and {b} is {a + b}.");
+
+
+
+Console.Write("Hello, Please enter your name: ");
+string name = Console.ReadLine();
+Console.Write("Please enter your age: ");
+int age = int.Parse(Console.ReadLine());
+Console.Write("Did you pass the driving theory test? (true/false): ");
+bool passedTheoryTest = bool.Parse(Console.ReadLine()); 
+
+//במידה והמשתמש בן 16 ומעלה ועבר את מבחן התיאוריה, נדפיס לו שהוא יכול לנהוג, אחרת נדפיס לו שהוא לא יכול לנהוג
+if (age >= 16 && passedTheoryTest)
+{
+    Console.WriteLine($"Hello {name}, you are old enough to drive!");
+}
+else
+{
+    Console.WriteLine($"Hello {name}, you are not old enough to drive.");
+}
+//אנחנו נבצע רק אם התנאי לא מתקיים else את הבלוק של ה 
---
+++ קבוצה 14/TIRGUL_1/Tar_Kita_1_Q1/Tar_KIta1_Q1/Program.cs
@@ -0,0 +1,18 @@
+﻿// כלל: סכום 2 צלעות במשולש חייב להיות גדול מהצלע השלישית
+
+Console.WriteLine("Side1:");
+double side1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Side2:");
+double side2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Side3:");
+double side3 = double.Parse(Console.ReadLine());
+
+if(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+{
+    Console.WriteLine("The triangle is valid.");
+}
+else
+{
+    Console.WriteLine("The triangle is not valid.");
+}
+
