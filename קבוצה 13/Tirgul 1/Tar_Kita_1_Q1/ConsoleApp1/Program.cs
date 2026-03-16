e40a8e3f1bf1a545c19f53c6ea61646d7efb7877
Shay Abraham <shaykos@gmail.com>
2026-03-16 11:43:09 +0200

    תרגול 1 קבוצה 13

---
+++ קבוצה 13/Tirgul 1/Tar_Kita_1_Q1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// סכום כל 2 צלעות במשולש חייב להיות גדול מהצלע השלישית
+
+Console.Write("side 1 = ");
+int side1 = int.Parse(Console.ReadLine());
+Console.Write("side 2 = ");
+int side2 = int.Parse(Console.ReadLine());
+Console.Write("side 3 = ");
+int side3 = int.Parse(Console.ReadLine());
+
+if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+{
+    Console.WriteLine("This is a triangle");
+}
+else
+{
+    Console.WriteLine("This is not a triangle");
+}
