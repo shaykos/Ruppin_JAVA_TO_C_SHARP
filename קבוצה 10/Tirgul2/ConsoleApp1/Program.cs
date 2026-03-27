523e25d350546f45c896e15b1a7981b7197f60c8
Shay Abraham <shaykos@gmail.com>
2026-03-27 12:02:42 +0300

    Add initial project and solution files for ConsoleApp1

---
+++ קבוצה 10/Tirgul2/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1 = 5, num2 = 7;
+        Console.WriteLine($"Before Swap1: num1 = {num1}, num2 = {num2}");
+        Swap1(num1, num2); // Call by Value
+        Console.WriteLine($"After Swap1: num1 = {num1}, num2 = {num2}");
+        Swap2(ref num1, ref num2); // Call by Reference
+        Console.WriteLine($"After Swap2: num1 = {num1}, num2 = {num2}");
+
+        int num3;
+        SetValue(out num3); // Call by Output
+        Console.WriteLine($"Value set by SetValue: num3 = {num3}");
+    }
+
+    static void Swap1(int a, int b)
+    {
+        Console.WriteLine($"Inside Swap1 (before swap): a = {a}, b = {b}");
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine($"Inside Swap1 (after swap): a = {a}, b = {b}");
+    }
+
+    static void Swap2(ref int a, ref int b)
+    {
+        Console.WriteLine($"Inside Swap2 (before swap): a = {a}, b = {b}");
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine($"Inside Swap2 (after swap): a = {a}, b = {b}");
+    }
+
+    static void SetValue(out int num3)
+    {
+        num3 = 10; // Must assign a value before exiting the method
+    }
+}
